fb1f91717cf50adee7129d4a86309aece177cb51
mheftie296 <45861926+mheftie296@users.noreply.github.com>
2025-02-17 22:57:57 -0600

    Improve 3D rendering, standard tank model

--- engine/Draw.js
+++ engine/Draw.js
@@ -1,6 +1,7 @@
 class Draw{
     static ly = 0.0
     static j = 0
+    static buffer = []
     static drawPoly(points){
         ctx.beginPath()
         ctx.fillStyle = points[0]
@@ -10,31 +11,46 @@ class Draw{
         }
         ctx.fill()
     }
+    static doDraw(){
+        //https://www.w3schools.com/js/js_array_sort.asp
+        
+        this.buffer.sort(function(a, b){return a[0] - b[0]})
+        this.buffer.reverse()
+        for(let point of this.buffer){
+            //point[1][0] = "#"+Math.round(100-(point[0]*5)).toString(16) + "0000"
+            Draw.drawPoly(point[1])
+        }
+        this.buffer = []
+    }
     static draw3d(poly){
         let dpoly = []
+        let dist = 0
+        let height = 0
         dpoly.push(poly[0])
+        let draw = false
+        for (const point of poly.slice(1)){
+            if(point[1]>0.1){
+                draw = true
+                break
+            }
+        }
+        if(!draw){
+            return
+        }
         for (const point of poly.slice(1)) {
-            //if(point[1]<0.1)
-            //    point[1] = 0.01
-            let distanceX = Math.sqrt(point[0]*point[0] + point[1]*point[1])
-            let angleX
-            if(point[0]<0)
-                angleX = -Math.acos(point[1]/distanceX)
-            else
-                angleX = Math.acos(point[1]/distanceX)
-            let distanceY = Math.sqrt(point[2]*point[2] + point[1]*point[1])
-            let angleY
-            if(point[1]<0)
-                angleY = Math.acos(point[1]/distanceY)
-            else
-                angleY = -Math.acos(point[1]/distanceY)
-                
-            let aX = Math.tan(angleX) * 400
-            let aY = Math.tan(angleY + Draw.ly) * 400
-            //let aY = Math.tan(Math.atan((point[2])/(point[1])) + Draw.ly) * 400
+            dist += (Math.sqrt(point[0]**2 + point[1]**2))
+            height += point[2]
+            if(point[1]<0.1)
+                point[1] = 0.01
+            let aX = Math.tan(Math.atan((point[0])/(point[1]))) * 400
+            let aY = Math.tan(Math.atan((point[2])/(point[1])) + Draw.ly) * 400
             dpoly.push([-aX,-aY])
         }
-        Draw.drawPoly(dpoly)
+        //console.log(dist.sort()[0])
+        let d = dist/poly.length + -20000 * height/poly.length
+        //console.log(d)
+        this.buffer.push([d, dpoly])
+        //Draw.drawPoly(dpoly)
     }
     static drawTank(poly, r){
         let newPoly = [poly[0]]
@@ -68,9 +84,22 @@ class Draw{
         for (const loc in p) {
             newPoly.push(this.rotate(p[loc][0], p[loc][1], r))
             newPoly[newPoly.length-1][1] += 4
-            newPoly[newPoly.length-1].push(-1.3 - Draw.j)
+            newPoly[newPoly.length-1].push(-1.3001 - Draw.j)
         }
         Draw.draw3d(newPoly)
+    }
+    static drawModel(model, r1, r2, location){
+        let loca = this.rotate(location[0], location[1], r1)
+        for(const poly of model){
+            let newPoly = [poly[0]]
+            for (const loc of poly.slice(1)) {
+                newPoly.push(this.rotate(loc[0], loc[1], r1 + r2))
+                newPoly[newPoly.length-1][0] += loca[0]
+                newPoly[newPoly.length-1][1] += 4 + loca[1]
+                newPoly[newPoly.length-1].push(loc[2] + location[2])
+            }
+            Draw.draw3d(newPoly)
+        }
     }
     static drawFlat2(p, r, c){
         let newPoly = [c]
--- game/MainScene.js
+++ game/MainScene.js
@@ -1,34 +1,19 @@
 class MainScene extends Scene{
     static lx = 0
     static ly = 0.1
-    static x = 0.01
-    static y = 0
+    static x = -10.01
+    static y = -10
     static tr = 0
-    static fx = 0
-    static fy = 0
+    static fx = -10.01
+    static fy = -10
     static inertia = 0
     static clx = 0
     static j = 0
-    static car = [
-        ["#0004", [-0.3,-0.5,-1.3],[-0.3,0.5,-1.3],[0.3,0.5,-1.3],[0.3,-0.5,-1.3]],
-        ["#222", [0.2,-0.4,-1.3],[0.2,0.4,-1.3],[0.3,0.4,-1.3],[0.3,-0.4,-1.3]],
-        ["#222", [0.3,-0.5,-1.2],[0.3,0.5,-1.2],[0.3,0.4,-1.3],[0.3,-0.4,-1.3]],
-        ["#222", [0.2,-0.5,-1.2],[0.2,0.5,-1.2],[0.2,0.4,-1.3],[0.2,-0.4,-1.3]],
-        ["#222", [0.2,-0.5,-1.2],[0.3,-0.5,-1.2],[0.3,-0.4,-1.3],[0.2,-0.4,-1.3]],
-
-        ["#222", [-0.2,-0.4,-1.3],[-0.2,0.4,-1.3],[-0.3,0.4,-1.3],[-0.3,-0.4,-1.3]],
-        ["#222", [-0.3,-0.5,-1.2],[-0.3,0.5,-1.2],[-0.3,0.4,-1.3],[-0.3,-0.4,-1.3]],
-        ["#222", [-0.2,-0.5,-1.2],[-0.2,0.5,-1.2],[-0.2,0.4,-1.3],[-0.2,-0.4,-1.3]],
-        ["#222", [-0.2,-0.5,-1.2],[-0.3,-0.5,-1.2],[-0.3,-0.4,-1.3],[-0.2,-0.4,-1.3]],
-
-        ["#161", [-0.3,-0.5,-1.2],[-0.3,0.5,-1.2],[0.3,0.5,-1.2],[0.3,-0.5,-1.2]],
-        ["#171",[-0.3,-0.5,-1.2],[-0.3,-0.4,-1.1],[0.3,-0.4,-1.1],[0.3,-0.5,-1.2]],
-        ["#171",[-0.3,0.5,-1.2],[-0.3,0.3,-1.1],[0.3,0.3,-1.1],[0.3,0.5,-1.2]],
-        ["#272", [-0.3,-0.4,-1.1],[-0.3,0.3,-1.1],[0.3,0.3,-1.1],[0.3,-0.4,-1.1]]
-        
-    ]
+    static spc = false
+    static bullets = []
+    
     start(){
-        //
+        MainScene.bullets.push([MainScene.fx, MainScene.fy, MainScene.clx])
     }
     update(){
         if(Input.keysdown.includes("ArrowLeft")){
@@ -57,6 +42,11 @@ class MainScene extends Scene{
             MainScene.clx -= 0.01
             MainScene.lx -= 0.05
         }
+        if(Input.keysdown.includes("Space") && !MainScene.spc){
+            MainScene.bullets.push([MainScene.fx, MainScene.fy, MainScene.clx])
+            MainScene.spc = true
+        }
+        if(!Input.keysdown.includes("Space")){MainScene.spc = false}
         MainScene.inertia -= 0.05
         if(MainScene.j + MainScene.inertia <= 0){
             MainScene.j = 0
@@ -65,18 +55,7 @@ class MainScene extends Scene{
         MainScene.j += MainScene.inertia
     }
     draw(){
-        let map = [[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1], 
-        [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
-        [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],  
-        [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
-        [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1], 
-        [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
-        [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1], 
-        [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
-        [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1], 
-        [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
-        [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1]
-    ]
+        
         ctx.clearRect(0, 0, canvas.width, canvas.height)
         let x = MainScene.x
         let y = MainScene.y
@@ -89,26 +68,64 @@ class MainScene extends Scene{
         MainScene.fy = (y + MainScene.fy*3)/4
         let fx = MainScene.fx
         let fy = MainScene.fy
-        //for (const i in map) {
-        //    for (const j in map[i])
-        //        if(map[i][j] == 1)
-        //            Draw.drawFlat([[i*s+fx,j*s+fy], [i*s+s+fx,j*s+fy], [i*s+s+fx,j*s+s+fy], [i*s+fx,j*s+s+fy]], MainScene.clx, "red")
-        //        else{
-        //            Draw.drawFlat([[i*s+fx,j*s+fy], [i*s+s+fx,j*s+fy], [i*s+s+fx,j*s+s+fy], [i*s+fx,j*s+s+fy]], MainScene.clx, "lightgray")
-        //        }
-        //}
         for (const i in map) {
-                for (const j in map[i])
-                    Draw.draw3d(["#333", [i*s+fx,j*s+fy, -1.3], [i*s+s+fx,j*s+fy, -1.3], [i*s+s+fx,j*s+s+fy, -1.3], [i*s+fx,j*s+s+fy, -1.3]])
+            for (const j in map[i]){
+                if(map[i][j] == 1)
+                    Draw.drawFlat([[i*s+fx,j*s+fy], [i*s+s+fx,j*s+fy], [i*s+s+fx,j*s+s+fy], [i*s+fx,j*s+s+fy]], MainScene.clx, "red")
+                else if(map[i][j] == 0){
+                    Draw.drawFlat([[i*s+fx,j*s+fy], [i*s+s+fx,j*s+fy], [i*s+s+fx,j*s+s+fy], [i*s+fx,j*s+s+fy]], MainScene.clx, "lightgray")
+                } else if((i*1 + j*1) % 2 == 0){
+                    Draw.drawModel(box, MainScene.clx, 0, [fx+i*1,fy+j*1,-1.3])
+                    Draw.drawModel(box, MainScene.clx, Math.PI/2, [fx+i*1+1,fy+j*1,-1.2])
+                    Draw.drawModel(box, MainScene.clx, 0, [fx+i*1,fy+j*1,-1.1])
+                    Draw.drawModel(box, MainScene.clx, Math.PI/2, [fx+i*1+1,fy+j*1,-1.0])
+                    Draw.drawModel(box, MainScene.clx, 0, [fx+i*1,fy+j*1,-0.9])
+                    Draw.drawModel(box, MainScene.clx, Math.PI/2, [fx+i*1+1,fy+j*1,-0.8])
+                    Draw.drawModel(box, MainScene.clx, Math.PI/2, [fx+i*1+1,fy+j*1,-1.25])
+                    Draw.drawModel(box, MainScene.clx, 0, [fx+i*1,fy+j*1,-1.15])
+                    Draw.drawModel(box, MainScene.clx, Math.PI/2, [fx+i*1+1,fy+j*1,-1.05])
+                    Draw.drawModel(box, MainScene.clx, 0, [fx+i*1,fy+j*1,-0.95])
+                    Draw.drawModel(box, MainScene.clx, Math.PI/2, [fx+i*1+1,fy+j*1,-0.85])
+                } else {
+                    Draw.drawModel(box, MainScene.clx, Math.PI/2, [fx+i*1+1,fy+j*1,-1.3])
+                    Draw.drawModel(box, MainScene.clx, 0, [fx+i*1,fy+j*1,-1.2])
+                    Draw.drawModel(box, MainScene.clx, Math.PI/2, [fx+i*1+1,fy+j*1,-1.1])
+                    Draw.drawModel(box, MainScene.clx, 0, [fx+i*1,fy+j*1,-1.0])
+                    Draw.drawModel(box, MainScene.clx, Math.PI/2, [fx+i*1+1,fy+j*1,-0.9])
+                    Draw.drawModel(box, MainScene.clx, 0, [fx+i*1,fy+j*1,-0.8])
+                    Draw.drawModel(box, MainScene.clx, 0, [fx+i*1,fy+j*1,-1.25])
+                    Draw.drawModel(box, MainScene.clx, Math.PI/2, [fx+i*1+1,fy+j*1,-1.15])
+                    Draw.drawModel(box, MainScene.clx, 0, [fx+i*1,fy+j*1,-1.05])
+                    Draw.drawModel(box, MainScene.clx, Math.PI/2, [fx+i*1+1,fy+j*1,-0.95])
+                    Draw.drawModel(box, MainScene.clx, 0, [fx+i*1,fy+j*1,-0.85])
+                }
+            }
         }
-        for(const poly in MainScene.car){
-            Draw.drawTank(MainScene.car[poly], MainScene.tr + MainScene.lx)
+        for(let bullet of MainScene.bullets){
+            bullet[1]-=Math.cos(-bullet[2])
+            bullet[0]+=Math.sin(-bullet[2])
+            let bx = -bullet[0]
+            let by = -bullet[1]
+            Draw.drawModel(bulletm, MainScene.clx, -bullet[2], [MainScene.fx+bx,MainScene.fy + by,0])
+            //Draw.drawFlat([[bx-0.1,by-0.1], [bx+0.1,by-0.1], [bx+0.1,by+0.1], [bx-0.1,by+0.1]], MainScene.clx-bullet[2 ], "Black")
+            //Draw.draw3d(["#FF0", [bx-0.02,by+4.25,-1.05], [bx+0.02,by+4.25,-1.05], [bx+0.02,by+5,-1.05], [bx-0.02,by+5,-1.05]])
+            //Draw.drawTank(["#fca400", [bx+MainScene.fx-0.02,by+MainScene.fy+0.5,-1.05], [bx+MainScene.fx+0.02,by+MainScene.fy+0.5,-1.05], [bx+MainScene.fx+0.02,by+MainScene.fy+1,-1.05], [bx+MainScene.fx-0.02,by+MainScene.fy+1,-1.05]], MainScene.clx-bullet[2])
+            //Draw.drawTank(["#de9000", [bx+MainScene.fx-0.02,by+MainScene.fy+0.5,-1.05], [bx+MainScene.fx+0.02,by+MainScene.fy+0.5,-1.05], [bx+MainScene.fx+0.02,by+MainScene.fy+0.5,-1.09], [bx+MainScene.fx-0.02,by+MainScene.fy+0.5,-1.09]], MainScene.clx-bullet[2])
+            //console.log("Drawing bullet, original location:", bullet, "offset:", [MainScene.fx, MainScene.fy])
         }
-        Draw.draw3d(["#272", [-0.02,4.25,-1.05], [0.02,4.25,-1.05], [0.02,5,-1.05], [-0.02,5,-1.05]])
-        Draw.draw3d(["#575", [-0.01,4.25,-1.05], [0.01,4.25,-1.05], [0.01,5,-1.05], [-0.01,5,-1.05]])
-        Draw.draw3d(["#252", [-0.25,3.75,-1.1], [0.25,3.75,-1.1], [0.25,4.25,-1.1], [-0.25,4.25,-1.1]])
-        Draw.draw3d(["#474", [-0.2,3.75,-1], [0.2,3.75,-1], [0.2,4.25,-1], [-0.2,4.25,-1]])
-        Draw.draw3d(["#363", [-0.25,3.75,-1.1], [0.25,3.75,-1.1], [0.2,3.75,-1], [-0.2,3.75,-1]])
-        
+        Draw.drawModel(tank, MainScene.clx, 0, [MainScene.fx+1.6,MainScene.fy + 3.05,0])
+        Draw.drawModel(turret, MainScene.clx, -0, [MainScene.fx+1.6,MainScene.fy + 3,0])
+        //for(const poly of car){
+        //    Draw.drawTank(poly, MainScene.tr + MainScene.lx)
+        //}
+        if(MainScene.spc)
+            Draw.draw3d(["#fff9", [-0.2,5,-1.15], [0.2,5,-0.99], [0.2,5,-1.15], [-0.2,5,-0.99]])
+        //Draw.draw3d(["#272", [-0.02,4.25,-1.05], [0.02,4.25,-1.05], [0.02,5,-1.05], [-0.02,5,-1.05]])
+        //Draw.draw3d(["#252", [-0.25,3.75,-1.09], [0.25,3.75,-1.09], [0.25,4.25,-1.09], [-0.25,4.25,-1.09]])
+        //Draw.draw3d(["#474", [-0.2,3.75,-1], [0.2,3.75,-1], [0.2,4.25,-1], [-0.2,4.25,-1]])
+        //Draw.draw3d(["#363", [-0.25,3.75,-1.1], [0.25,3.75,-1.1], [0.2,3.75,-1], [-0.2,3.75,-1]])
+        Draw.drawModel(tank, MainScene.tr + MainScene.lx, 0, [0,0.05,0])
+        Draw.drawModel(turret, 0, 0, [0,-0.01,0])
+        Draw.doDraw()
     }
 }
